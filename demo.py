f213178ca46c5a581ac667432ee6b76ebc771bc4
root <moeinzjg@gmail.com>
2021-11-28 22:33:51 +0000

    Added eval and demo scripts

---
+++ demo.py
@@ -0,0 +1,43 @@
+import torch
+import pytorch_mask_rcnn as pmr
+
+
+use_cuda = True
+dataset = "coco"
+ckpt_path = "maskrcnn_coco-25.pth"
+data_dir = "../Vegas_coco_random_splits/"
+
+device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
+if device.type == "cuda":
+    pmr.get_gpu_prop(show=True)
+print("\ndevice: {}".format(device))
+
+ds = pmr.datasets(dataset, data_dir, "test", train=True)
+d = torch.utils.data.DataLoader(ds, shuffle=False)
+
+model = pmr.maskrcnn_resnet50(False, max(ds.classes) + 1).to(device)
+model.eval()
+model.head.score_thresh = 0.3
+
+if ckpt_path:
+    checkpoint = torch.load(ckpt_path, map_location=device)
+    model.load_state_dict(checkpoint["model"])
+    del checkpoint
+
+for p in model.parameters():
+    p.requires_grad_(False)
+
+iters = 6
+
+for i, (image, target) in enumerate(d):
+    image = image.to(device)[0]
+    target = {k: v.to(device) for k, v in target.items()}
+
+    with torch.no_grad():
+        result = model(image)
+    # TODO: add quatitative metrics for edge and vertex
+    # print(result)
+    pmr.show(image, result, ds.classes, "./images/output{}.jpg".format(i))
+
+    if i >= iters - 1:
+        break
