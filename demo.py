8e6b9449973736f777dcdd0c0aeee666ee93723c
root <moeinzjg@gmail.com>
2022-01-17 20:11:10 +0000

    Added arguments for demo.py

--- demo.py
+++ demo.py
@@ -1,60 +1,62 @@
+import os
+import argparse
+
 import torch
 import pytorch_mask_rcnn as pmr
 
 
-use_cuda = True
-dataset = "coco"
-ckpt_path = "maskrcnn_coco-15.pth"
-data_dir = "../Vegas_coco_random_splits/"
+def main(args):
+    use_cuda = True
+    dataset = args.dataset
+    ckpt_path = args.ckpt_path
+    data_dir = args.data_dir
 
-device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
-if device.type == "cuda":
-    pmr.get_gpu_prop(show=True)
-print("\ndevice: {}".format(device))
+    device = torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu")
+    if device.type == "cuda":
+        pmr.get_gpu_prop(show=True)
+    print("\ndevice: {}".format(device))
 
-ds = pmr.datasets(dataset, data_dir, "test", train=True)
-d = torch.utils.data.DataLoader(ds, shuffle=False)
+    ds = pmr.datasets(dataset, data_dir, "test", train=False)
+    d = torch.utils.data.DataLoader(ds, shuffle=True)
 
-model = pmr.maskrcnn_resnet50(False, max(ds.classes) + 1).to(device)
-model.eval()
-model.head.score_thresh = 0.3
+    model = pmr.maskrcnn_resnet50(False, max(ds.classes) + 1).to(device)
+    model.eval()
+    model.head.score_thresh = 0.3
 
-if ckpt_path:
-    checkpoint = torch.load(ckpt_path, map_location=device)
-    model.load_state_dict(checkpoint["model"])
-    del checkpoint
+    if ckpt_path:
+        checkpoint = torch.load(ckpt_path, map_location=device)
+        model.load_state_dict(checkpoint["model"])
+        del checkpoint
 
-for p in model.parameters():
-    p.requires_grad_(False)
+    for p in model.parameters():
+        p.requires_grad_(False)
 
-# Quantitative measures for edge and vertex
-# for i, (image, target) in enumerate(d):
-#     image = image.to(device)[0]
-#     target = {k: v.to(device) for k, v in target.items()}
+    # Visualization
+    num_images = args.num_img
 
-#     with torch.no_grad():
-#         result = model(image)
-    
-#     prediction = {k: v.cpu() for k, v in result.items()}
+    for i, (image, target) in enumerate(d):
+        image = image.to(device)[0]
+        # target.pop('global_polygons')
+        target = {k: v.to(device) for k, v in target.items()}
 
-#     # pred_edges = prediction['edges'].numpy()
-#     # pred_vertices = prediction['vertices'].numpy()
+        with torch.no_grad():
+            result = model(image)
+        pmr.show(image, result, ds.classes, "./images/output{}.jpg".format(i))
 
-#     # gt_edges = target['edges'].cpu().numpy()
-#     # gt_vertices = target['vertices'].cpu().numpy()
-#     # Average Precision
+        if i >= num_images - 1:
+            break
 
-# Visualization
-num_images = 6
 
-for i, (image, target) in enumerate(d):
-    image = image.to(device)[0]
-    target.pop('global_polygons')
-    target = {k: v.to(device) for k, v in target.items()}
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="coco")
+    parser.add_argument("--data_dir", default="../Vegas_coco_random_splits")
+    parser.add_argument("--ckpt_path", default="maskrcnn_coco-25.pth")
+    parser.add_argument("--num_img", default=3)
+    args = parser.parse_args()
 
-    with torch.no_grad():
-        result = model(image)
-    pmr.show(image, result, ds.classes, "./images/output{}.jpg".format(i))
+    args.use_cuda = True
+    args.results = os.path.join(os.path.dirname(args.ckpt_path), "maskrcnn_results.pth")
+    args.rpolygcn_results = os.path.join(os.path.dirname(args.ckpt_path), "rpolygcn_results.pth")
 
-    if i >= num_images - 1:
-        break
+    main(args)
